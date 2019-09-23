ad027d2042bbf26c30a7e6c977e08b74f4f72eab
mvishak <vishal.murthy@gmail.com>
2019-09-23 13:51:07 +0530

    changed

--- com/chatbot/data/networkschedule.py
+++ com/chatbot/data/networkschedule.py
@@ -1,1 +1,5 @@
-class networkschedule
+class NetworkSchedule(object)
+    def__init__(self, recordType, upc, category):
+    self.recordType = recordType
+    self.upc = upc
+    self.category = category
